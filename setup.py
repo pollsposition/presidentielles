b7eb1665cea9dc328c33df9bf64dc991b39fb1e7
Rémi Louf <remilouf@gmail.com>
2021-09-06 17:57:20 +0200

    add Ipsos-Sopra Steria 03/09/2021

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pollsposition',
+    packages=find_packages(),
+    version='0.0.0',
+    description='Package used at pollsposition',
+    author='Remi Louf',
+    license='GPL',
+)
